8f101019a776d315343ae5c7b258a9e9bca9a184
esteban1234 <tva_libra@hotmail.com>
2017-10-27 11:12:34 -0500

    metas

--- vistas/encabezado.php
+++ vistas/encabezado.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="Qji5TcySRn7eq2vU2U3LX2pcsMnkcW0JCwv25mMp5JI" />
 	<link rel="icon" type="image/png" href="img/favicon.png" />
 	<title>KEMPO COMERCIALIZADORA MAYORISTA</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
